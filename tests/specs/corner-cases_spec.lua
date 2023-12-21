79932518430b2ac1848b4bdb2c016526b35990b4
Vlad Chepaykyn <backdround@yandex.ru>
2023-12-21 07:36:46 +0300

    Add corner case
    A hop from the middle of a word should work now

---
+++ tests/specs/corner-cases_spec.lua
@@ -0,0 +1,14 @@
+local nw = require("neowords")
+local h = require("tests.helpers")
+
+require("tests.custom-asserts").register()
+
+describe("corner-cases", function()
+  it("hop forward_end from the middle of a word", function()
+    h.get_preset("several_sneak_words", { 1, 2 })()
+    local hops = nw.get_word_hops("\\v[[:lower:]]+")
+
+    h.perform_through_keymap(hops.forward_end, true)
+    assert.cursor_at(1, 6)
+  end)
+end)
