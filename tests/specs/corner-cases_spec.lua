659ff482e80e82424756654c495f0bbfa68db58c
Vlad Chepaykyn <backdround@yandex.ru>
2024-01-03 11:14:18 +0300

    Add special case

--- tests/specs/corner-cases_spec.lua
+++ tests/specs/corner-cases_spec.lua
@@ -11,4 +11,34 @@ describe("corner-cases", function()
     h.perform_through_keymap(hops.forward_end, true)
     assert.cursor_at(1, 6)
   end)
+
+  local description =
+    "hop from the place that doesn't match the start of a word, but is part of a word"
+  describe(description, function()
+    before_each(h.get_preset([[
+      -a-w- a---w -a-w-
+    ]], { 1, 8 }))
+
+    local hops = nw.get_word_hops("\\v-@![-[:lower:]]+")
+
+    it("forward_start", function()
+      h.perform_through_keymap(hops.forward_start, true)
+      assert.cursor_at(1, 13)
+    end)
+
+    it("forward_end", function()
+      h.perform_through_keymap(hops.forward_end, true)
+      assert.cursor_at(1, 10)
+    end)
+
+    it("backward_start", function()
+      h.perform_through_keymap(hops.backward_start, true)
+      assert.cursor_at(1, 6)
+    end)
+
+    it("backward_end", function()
+      h.perform_through_keymap(hops.backward_end, true)
+      assert.cursor_at(1, 4)
+    end)
+  end)
 end)
